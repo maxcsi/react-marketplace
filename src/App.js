2d7359404ef20e97472d211a5bdb38108d812fb8
Max Ferreira <maxexatas@gmail.com>
2021-12-16 13:35:31 -0300

    Adiciona firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Router, Route } from "react-router-dom";
+
+import ListItem from "./components/ListItem/ListItem";
+import ShoppingBag from "./assets/icons/ShoppingBag";
+
 function App() {
-  return <h1> Hello Word!</h1>
+  return (
+    <div className="main-application">
+      <header>
+        <div>
+          <ShoppingBag />
+          <h2>Max Store</h2>
+        </div>
+
+        <nav>
+          <ul>
+            <ListItem label="Products" />
+            <ListItem label="Shopping Cart" />
+            <ListItem label="Sign in" />
+          </ul>
+        </nav>
+      </header>
+
+      <Router></Router>
+    </div>
+  );
 }
 
 export default App;
